78440676e81bbf78aeb29c545f03b20718424fd8
mihsanalam <mdmihsan2@gmail.com>
2026-09-20 22:31:57 +0600

    Home: move Experience section to third position after About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
       <main>
         <Hero />
         <About />
+        <Experience />
         <Projects />
         <Skills />
-        <Experience />
         <Testimonials />
         <Contact />
       </main>
