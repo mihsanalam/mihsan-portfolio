e9b143b200e7c4d30520adf8ec279cc48d73b01a
mihsanalam <mdmihsan2@gmail.com>
2026-09-21 22:44:09 +0600

    UI: remove scroll indicator from hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -82,21 +82,6 @@ export default function Hero() {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-      >
-        <motion.div
-          animate={{ y: [0, 8, 0] }}
-          transition={{ duration: 2, repeat: Infinity }}
-          className="w-5 h-8 border-2 border-border rounded-full flex justify-center pt-1.5"
-        >
-          <div className="w-1 h-1.5 bg-accent rounded-full" />
-        </motion.div>
-      </motion.div>
     </section>
   );
 }
